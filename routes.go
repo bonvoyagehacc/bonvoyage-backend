24a764854ebc07ced3d8ab6a8a6106c3b0f0c86f
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-07-16 19:46:27 -0400

    responses and request reading

---
+++ routes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "net/http"
+    "encoding/json"
+    "fmt"
+)
+
+type credRequest struct {
+    Username string
+    Password string
+}
+
+type tokenResponse struct {
+    AccessToken string
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+
+    var creds credRequest
+    err := json.NewDecoder(r.Body).Decode(&creds)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    fmt.Println(creds)
+
+    /* return access token */
+    w.Header().Add("content-type", "application/json")
+    token := tokenResponse{AccessToken: "peepeepoopoo"}
+    json.NewEncoder(w).Encode(token)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+}
+
+func Routes() {
+    // http.HandleFunc("/photo", photoHandler);
+    http.HandleFunc("/auth/login", loginHandler);
+    http.HandleFunc("/auth/register", registerHandler);
+}
+
--- server.go
+++ server.go
@@ -2,33 +2,10 @@ package main
 
 import (
     "net/http"
-    "encoding/json"
 )
 
-type Photo struct {
-    Name string
-    Description string
-}
-
-func photoHandler(w http.ResponseWriter, r *http.Request) {
-    photoEntry := Photo{Name: "girl's last tour", Description: "potato"}
-    jsonBytes, err := json.Marshal(photoEntry)
-
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(err.Error()))
-    }
-
-    w.Header().Add("content-type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonBytes)
-}
-
-func routes() {
-    http.HandleFunc("/photo", photoHandler);
-}
-
 func main() {
-    routes()
+    Routes()
     http.ListenAndServe(":8080", nil)
 }
+
