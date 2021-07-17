e998551b4cf95502715d250feba9844d91a93be5
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-07-16 22:19:22 -0400

    basic sql queries

--- db.go
+++ db.go
@@ -14,11 +14,49 @@ type PostgresConnection struct {
     DBName string
 }
 
+// CREATE TABLE users(
+//     id serial primary key,
+//     username varchar(64) not null,
+//     password varchar(128) not null
+// );
+
+type PostgresDB struct {
+    db *sql.DB
+}
+
+func (p *PostgresDB) RegisterUser(username string, password string) {
+    db := p.db
+
+    query := `
+    INSERT INTO users (username, password)
+    VALUES ($1, $2)
+    RETURNING id
+    `
+    _, err := db.Exec(query, username, password)
+    if err != nil {
+        panic(err)
+    }
+    return
+}
+
+func (p *PostgresDB) AuthenticateUser(username string, password string) {
+    db := p.db
+
+    query := `
+    SELECT id FROM users WHERE username = $1 AND password = $2
+    `
+    _, err := db.Exec(query, username, password)
+    if err != nil {
+        panic(err)
+    }
+    return
+}
+
 func main() {
 
     connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", ConnectionSecrets.Host, ConnectionSecrets.Port, ConnectionSecrets.User, ConnectionSecrets.Password, ConnectionSecrets.DBName)
 
-    /* validat connection */
+    /* validate connection */
     db, err := sql.Open("postgres", connectionString)
     if err != nil {
         panic(err)
@@ -29,6 +67,9 @@ func main() {
     if err != nil {
         panic(err)
     }
+
+    psdb := &PostgresDB{db: db}
+    psdb.RegisterUser("pinosaur", "peepee")
 
     fmt.Println("Connected to database")
 }
