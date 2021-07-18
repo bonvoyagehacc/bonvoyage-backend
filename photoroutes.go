945f8f4fad2e9bda29bb5217c0319006e3a3a7a4
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2021-07-18 00:08:15 -0400

    Update photoroutes.go

--- photoroutes.go
+++ photoroutes.go
@@ -1,60 +1,71 @@
 package main
 
 import (
-    "bytes"
-    "path/filepath"
-    "archive/zip"
-    "io/ioutil"
-    "net/http"
-    "github.com/pixolous/pixolousAnalyze"
+	"archive/zip"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+
+	"github.com/pixolous/pixolousAnalyze"
 )
+
+type galleryResponse struct {
+	Images []string `json:"image"`
+}
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
-    /* unzip received data */
-    raw, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        http.Error(w, "malformed zip file", http.StatusBadRequest); return
-    }
-    reader, err := zip.NewReader(bytes.NewReader(raw), int64(len(raw)))
-    if err != nil {
-        http.Error(w, "malformed zip file", http.StatusBadRequest); return
-    }
+	/* unzip received data */
+	raw, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "malformed zip file", http.StatusBadRequest)
+		return
+	}
+	reader, err := zip.NewReader(bytes.NewReader(raw), int64(len(raw)))
+	if err != nil {
+		http.Error(w, "malformed zip file", http.StatusBadRequest)
+		return
+	}
 
-    /* get userid from context */
-    userid := r.Context().Value("userid")
-    if userid == nil {
-        w.WriteHeader(http.StatusInternalServerError); return
-    }
-    /* get user's hash */
-    userhash, err := GetUserHash(userid.(int))
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError); return
-    }
+	/* get userid from context */
+	userid := r.Context().Value("userid")
+	if userid == nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	/* get user's hash */
+	userhash, err := GetUserHash(userid.(int))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-    /* get list of files */
-    for _, file := range reader.File {
-        ext := filepath.Ext(file.Name)
-        filehash := GenerateMD5(file.Name)
+	/* get list of files */
+	for _, file := range reader.File {
+		ext := filepath.Ext(file.Name)
+		filehash := GenerateMD5(file.Name)
 
-        /* write photo to drive */
-        WriteImageFile(file, userhash, filehash+ext)
+		/* write photo to drive */
+		WriteImageFile(file, userhash, filehash+ext)
 
-        /* compute ahash */
-        ahash := pixolousAnalyze.AHash(filepath.Join(ResourceDir, userhash, filehash+ext))
+		/* compute ahash */
+		ahash := pixolousAnalyze.AHash(filepath.Join(ResourceDir, userhash, filehash+ext))
 
-        /* write photo to db */
-        NewPhoto(userid.(int), filehash+ext, ahash)
-    }
+		/* write photo to db */
+		NewPhoto(userid.(int), filehash+ext, ahash)
+	}
 }
 
 func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	images := galleryResponse{Images: []string{"pinosaur", "is", "really", "really", "gay", "but", "so", "is", "golang"}}
 
-
+	w.Header().Add("content-type", "application/json")
+	json.NewEncoder(w).Encode(images)
 }
 
 func PhotoRoutes(mux *http.ServeMux) {
-    mux.Handle("/photo/upload", RestrictMethod("POST", RestrictAuth(http.HandlerFunc(uploadHandler))));
-    mux.Handle("/photo/gallery", RestrictMethod("GET", RestrictAuth(http.HandlerFunc(galleryHandler))));
+	mux.Handle("/photo/upload", RestrictMethod("POST", RestrictAuth(http.HandlerFunc(uploadHandler))))
+	mux.Handle("/photo/gallery", RestrictMethod("GET", RestrictAuth(http.HandlerFunc(galleryHandler))))
 }
-
